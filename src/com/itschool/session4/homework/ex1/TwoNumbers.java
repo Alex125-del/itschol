f3d257149bc708f73dc3e1cd8e362a11e82cebc4
Alex125-del <alexandru.molovata.92@gmail.com>
2024-12-22 19:02:12 +0100

    add salut

---
+++ src/com/itschool/session4/homework/ex1/TwoNumbers.java
@@ -0,0 +1,26 @@
+package com.itschool.session4.homework.ex1;
+
+public class TwoNumbers {
+
+    static int number1 = 25;
+
+    static void method2(){
+        int secondNumber = 10;
+        System.out.println(number1 + secondNumber);
+        System.out.println(number1 - secondNumber);
+        System.out.println(number1 * secondNumber);
+        System.out.println(number1 % secondNumber);
+
+    }
+
+    public static void main(String[] args) {
+
+       method2();
+
+
+
+
+    }
+
+
+}
