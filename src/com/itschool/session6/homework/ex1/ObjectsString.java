f4ed6aa07948863bb6d99589b449a18e9a8e7b49
Alex125-del <alexandru.molovata.92@gmail.com>
2025-01-11 20:36:03 +0100

    11.01.25

---
+++ src/com/itschool/session6/homework/ex1/ObjectsString.java
@@ -0,0 +1,14 @@
+package com.itschool.session6.homework.ex1;
+
+public class ObjectsString {
+
+    public static void main(String[] args) {
+
+
+        String   stringOne = "OpenAI";
+        String  stringTwo = "OpenAI";
+
+        System.out.println("stringOne is the same object as stringTwo" + stringOne.equals(stringTwo));
+
+    }
+}
